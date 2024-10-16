d92b315bc3123784a627bae95268df519d74b37b
Albin Pious <albinpious65@gmail.com>
2024-10-16 09:16:00 +0530

    updated config

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -6,7 +6,7 @@ const config = {
     dbConnectionString: process.env.DB_CONNECTION_STRING,
     frontendURL: process.env.FRONTEND_URL,
     corsOptions: {
-        origin: process.env.FRONTEND_URL,
+        origin: '*',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
         allowHeaders: ['Authorization', 'Content-Type']
