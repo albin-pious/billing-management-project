298c0106eaf91830ef89d686665296fb40af5dbc
Albin Pious <albinpious65@gmail.com>
2024-10-16 08:44:24 +0530

    added env for frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: "http://127.0.0.1:5000/api",
+    baseURL: import.meta.env.VITE_BASE_URL,
     withCredentials: true,
 });
 
-export const BASE_URL = "http://127.0.0.1:5000"
-
+export const BASE_URL = import.meta.env.VITE_BASE_URL
+console.log(BASE_URL);
 // Request interceptor to attach token to headers
 api.interceptors.request.use(
     config => {
