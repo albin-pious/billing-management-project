d7cdf61d28dd510d53e7f356ef339bd532ef81e7
Albin Pious <albinpious65@gmail.com>
2024-10-16 14:16:48 +0530

    added multiple origins

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -6,7 +6,7 @@ const config = {
     dbConnectionString: process.env.DB_CONNECTION_STRING,
     frontendURL: process.env.FRONTEND_URL,
     corsOptions: {
-        origin: process.env.FRONTEND_URL || "*",
+        origin:[process.env.FRONTEND_URL, 'http://localhost:3000'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
         allowHeaders: ['Authorization', 'Content-Type']
